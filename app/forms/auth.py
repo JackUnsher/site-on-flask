eb248a872b1654975ed5c77cfb21fa97daeff2d3
User Name <user@example.com>
2025-04-28 20:04:25 +0400

    Добавлены формы аутентификации (LoginForm и RegistrationForm)

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -1,1 +1,44 @@
- 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from app.models.user import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[
+        DataRequired(),
+        Email()
+    ])
+    password = PasswordField('Пароль', validators=[
+        DataRequired()
+    ])
+    remember = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[
+        DataRequired(),
+        Length(min=3, max=64)
+    ])
+    email = StringField('Email', validators=[
+        DataRequired(),
+        Email()
+    ])
+    password = PasswordField('Пароль', validators=[
+        DataRequired(),
+        Length(min=6)
+    ])
+    password2 = PasswordField('Подтвердите пароль', validators=[
+        DataRequired(),
+        EqualTo('password', message='Пароли должны совпадать')
+    ])
+    submit = SubmitField('Зарегистрироваться')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Это имя пользователя уже занято')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Этот email уже зарегистрирован') 
